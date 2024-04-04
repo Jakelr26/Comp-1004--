770e681d93e45a00d9dc45b37c59bfc122af9ff1
Jakelr26 <146326562+Jakelr26@users.noreply.github.com>
2024-04-04 16:07:59 +0100

    still work s

--- Aims.js
+++ Aims.js
@@ -12,7 +12,7 @@ const APP = {
             }
           });
           APP.loadAims();
-
+          APP.newArchive();
     },
 
     saveAim(ev){
@@ -56,20 +56,74 @@ const APP = {
          //Tick symbol button stuff
          let elementList = document.getElementsByClassName('checkList');
          for (let i = 0; i < elementList.length; i++){
-            let span = document.createElement('span');
+            let span = document.createElement('SPAN');
             let TickSymbol = document.createTextNode('\u2714')
             span.className = 'close';
             span.appendChild(TickSymbol);
             elementList[i].appendChild(span);
          } 
+         
          let list = document.querySelector('.WriteAims');
          list.addEventListener('click', function(ev) {
            if (ev.target.tagName === 'LI') {
              ev.target.classList.toggle('checked');
            }
          }, false);
-        
+
+         let close = document.getElementsByClassName('close');
+         for (let i = 0; i < close.length; i++){
+            close[i].onclick = function(){
+                this.parentElement.style.display= 'none'; 
+
+
+                console.log(obj[i]);
+                APP.newArchive(obj[i]); 
+            }
+         }
+
+
+
+
     },
+
+    newArchive(newItem){
+
+        if (newItem){
+            let arc = newItem;
+
+            let key = 'aims-archive'; 
+            let storage = localStorage.getItem(key);
+            let ARCHS = [];
+            if (storage){
+                ARCHS = JSON.parse(storage);
+            }
+            ARCHS.push(arc);
+            localStorage.setItem(key, JSON.stringify(ARCHS));
+//            document.getElementById('Aim').value = ''
+//            APP.loadAims();
+            
+        }
+
+
+        let key = 'aims-archive';
+        let storage = localStorage.getItem(key);
+        obj = JSON.parse(storage);
+
+        let archSpace = document.getElementById('storeAims');
+        storeAims.innerHTML = '';
+        let DocumentFragment = document.createDocumentFragment();
+
+       //for each loop to append the local storage array to document fragment.
+        obj.forEach((objs) => {
+           let li = document.createElement('li');
+           li.textContent = objs;
+           li.className = 'archiveList'
+           DocumentFragment.appendChild(li);
+        }); 
+                   
+        archSpace.appendChild(DocumentFragment);
+
+    }
 };
 
 document.addEventListener('DOMContentLoaded', APP.init);
